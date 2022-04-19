d0585c3e1e326b8a0be1706a039e0c3e75403784
Mirabelle Abdulmassih <mirabelle.abdulmassih@epita.fr>
2022-04-19 17:00:51 +0200

    feat: add live and accuracy test modes

--- src/main.py
+++ src/main.py
@@ -48,6 +48,33 @@ def create_kmeans(super_sample):
     kmeans = KMeans(n_clusters=24, random_state=42)
     kmeans.fit(super_sample)
     return kmeans
+    
+def create_model(img_train, label_train):
+    masks_train = [create_mask(img) for img in img_train]
+
+    super_sample = create_super_sample(img_train, masks_train)
+
+    print('Training Kmeans')
+    kmeans = create_kmeans(super_sample)
+
+    # color_lut = np.uint8(kmeans.cluster_centers_)
+    print('Creating histograms')
+    labels_maps_train = [kmeans.predict(img[mask]) for (img, mask) in zip(img_train, masks_train)]
+    color_histograms_train = np.array([np.bincount(lm, minlength=len(kmeans.cluster_centers_)) / len(lm) for lm in labels_maps_train], dtype=np.float64)
+
+    def model(img_test):
+        masks_test = [create_mask(img) for img in img_test]
+        labels_maps_test = [kmeans.predict(img[mask]) for (img, mask) in zip(img_test, masks_test)]
+        color_histograms_test = np.array([np.bincount(lm, minlength=len(kmeans.cluster_centers_)) / len(lm) for lm in labels_maps_test], dtype=np.float64)
+        
+        dist_mat = cdist(color_histograms_test, color_histograms_train, metric='cosine')
+        idx_of_best_matches_per_row = np.argsort(dist_mat, axis=1)
+
+        indexes = idx_of_best_matches_per_row[:, 0]
+        return np.array(label_train)[indexes]
+
+    return model
+
 
 # def quantize_img_color(kmeans, img, mask, color_lut):
 #     label_map = kmeans.predict(img[mask])
@@ -55,30 +82,55 @@ def create_kmeans(super_sample):
 #     recolored_img[mask] = color_lut[label_map]
 #     return recolored_img
 
-def main(dataset_path):
+def load_test_images(path):
+    img_test, filenames = [], []
+
+    for filename in os.listdir(path):
+        complete_path = os.path.join(path, filename)
+
+        raw = cv2.imread(complete_path)
+        img = cv2.cvtColor(raw, cv2.COLOR_BGR2RGB)
+
+        img_test.append(img)
+        filenames.append(filename)
+
+    return img_test, filenames
+
+def dump_results(results, filenames_test, path):
+    with open(path, 'w') as file:
+        for filename, predicted_class_id in zip(filenames_test, results):
+            line = f'{filename},{predicted_class_id}\n'
+            file.write(line)
+
+def live(dataset_path, test_path):
+    np.random.seed(42)
+
+    img_train, label_train = load_dataset(dataset_path)
+
+    model = create_model(img_train, label_train)
+
+    img_test, filenames_test = load_test_images(test_path)
+    results = model(img_test)
+
+    dump_results(results, filenames_test, "results.txt")
+
+def accuracy_test(dataset_path):
     np.random.seed(42)
 
     train, labels = load_dataset(dataset_path)
-    X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.2, random_state=42, stratify=labels)
+    img_train, img_test, label_train, label_test = train_test_split(train, labels, test_size=0.2, random_state=42, stratify=labels)
 
-    masks_train = [create_mask(img) for img in X_train]
-    masks_test = [create_mask(img) for img in X_test]
+    model = create_model(img_train, label_train)
 
-    super_sample = create_super_sample(X_train, masks_train)
+    results = model(img_test)
 
-    kmeans = create_kmeans(super_sample)
+    accuracy = np.sum(np.array(results) == np.array(label_test)) / len(results)
 
-    # color_lut = np.uint8(kmeans.cluster_centers_)
-    labels_maps_train = [kmeans.predict(img[mask]) for (img, mask) in zip(X_train, masks_train)]
-    color_histograms_train = np.array([np.bincount(lm, minlength=len(kmeans.cluster_centers_)) / len(lm) for lm in labels_maps_train], dtype=np.float64)
-    
-    labels_maps_test = [kmeans.predict(img[mask]) for (img, mask) in zip(X_test, masks_test)]
-    color_histograms_test = np.array([np.bincount(lm, minlength=len(kmeans.cluster_centers_)) / len(lm) for lm in labels_maps_test], dtype=np.float64)
-    
-    dist_mat = cdist(color_histograms_test, color_histograms_train, metric='cosine')
-    
+    print("accuracy:", accuracy)
 
+def main(dataset_path, test_path):
+    live(dataset_path, test_path)
+    accuracy_test(dataset_path)
 
 if __name__ == "__main__":
-    main("../dataset/train")
-    
+    main("../dataset/train", "../tests")
