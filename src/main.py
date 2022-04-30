968ca22a0ddfdb961175cf89657bc703d428b53b
Jiayi Hao <jiayi.hao@epita.fr>
2022-04-30 17:03:57 +0200

    [ADD] Late fusion (0.6-color, 0.4-shape), implement early fusion function out of __main__

--- src/main.py
+++ src/main.py
@@ -76,9 +76,53 @@ def train(classifer, feature_histogram, label_train):
     # TODO: Cross validation ?
     classifer.fit(feature_histogram, label_train)
 
+def early_fusion(img_train, label_train, img_test, label_test, color_feature_extractor, shape_feature_extractor):
+    fused_feature_extractor = feature_extractor_fusion(color_feature_extractor, shape_feature_extractor)
+
+    print("Train classifier on features ...\n")
+    classifiers = {"knn_classifier": KNeighborsClassifier(n_neighbors=N_NEIGHBORS), "dummy_classifier": DummyClassifier()}
+
+    for name, clf in classifiers.items():
+        train(clf, fused_feature_extractor(img_train), label_train)
+        prediction, accuracy = evaluate(clf, fused_feature_extractor(img_test), label_test)
+        print(f"accuracy = {accuracy}\n")
+        dump_results(prediction, label_test, f"{name}-results")
+
+def get_max_proba_indices(color_predict_proba, shape_predict_proba):
+    classes_pred_proba = 0.6 * color_predict_proba + 0.4 * shape_predict_proba
+    return np.argmax(classes_pred_proba, axis=1)
+
+def soft_voting(color_predict_proba, shape_predict_proba, color_labels, shape_labels):
+    max_proba_indces = get_max_proba_indices(color_predict_proba, shape_predict_proba)
+    
+    # TODO stop cheating
+    return max_proba_indces + 1
+
+def late_fusion(img_train, label_train, img_test, label_test, color_feature_extractor, shape_feature_extractor):
+    classifiers = {"knn_clf-knn_clf": [KNeighborsClassifier(n_neighbors=N_NEIGHBORS), KNeighborsClassifier(n_neighbors=N_NEIGHBORS)],
+                   "dummy_clf-dummy_clf": [DummyClassifier(), DummyClassifier()]}
+
+    for name, clf in classifiers.items():
+        print(f"Train {name} classifier on color features ...\n")
+        train(clf[0], color_feature_extractor(img_train), label_train)
+        print(f"Train {name} classifier on shape features ...\n")
+        train(clf[1], shape_feature_extractor(img_train), label_train)
+
+        color_labels = clf[0].predict(color_feature_extractor(img_test))
+        shape_labels = clf[1].predict(shape_feature_extractor(img_test))
+        prediction = soft_voting(clf[0].predict_proba(color_feature_extractor(img_test)), clf[1].predict_proba(shape_feature_extractor(img_test)), color_labels, shape_labels)
+
+        accuracy = get_accuracy(prediction, label_test)
+        print(f"accuracy = {accuracy}\n")
+        dump_results(prediction, label_test, f"{name}-results")
+
+def get_accuracy(prediction, label):
+    accuracy = np.sum(np.array(prediction) == np.array(label)) / len(prediction)
+    return accuracy
+
 def evaluate(classifier, feature_histogram, label):
     prediction = classifier.predict(feature_histogram)
-    accuracy = np.sum(np.array(prediction) == np.array(label)) / len(prediction)
+    accuracy = get_accuracy(prediction, label)
     return prediction, accuracy
 
 if __name__ == "__main__":
@@ -90,17 +134,11 @@ if __name__ == "__main__":
 
     print("Create feature extractor ...\n")
     color_feature_extractor, shape_feature_extractor = feature_extractor(img_train)
+
+    # Early fusion
+    early_fusion(img_train, label_train, img_test, label_test, color_feature_extractor, shape_feature_extractor)
     
-    # FIXME: Vstack seems to work as expected but investigate why it breaks on train().
-    fused_feature_extractor = feature_extractor_fusion(color_feature_extractor, shape_feature_extractor)
+    # Late fusion
+    late_fusion(img_train, label_train, img_test, label_test, color_feature_extractor, shape_feature_extractor)
+    
 
-    print("Train classifier on features ...\n")
-    # classifiers = {"dummy_classifier" : DummyClassifier(), "knn_classifier": KNeighborsClassifier(n_neighbors=1)}
-    # classifiers = {"dummy_classifier" : DummyClassifier()}
-    classifiers = {"knn_classifier": KNeighborsClassifier(n_neighbors=N_NEIGHBORS), "dummy_classifier": DummyClassifier()}
-
-    for name, clf in classifiers.items():
-        train(clf, fused_feature_extractor(img_train), label_train)
-        prediction, accuracy = evaluate(clf, fused_feature_extractor(img_test), label_test)
-        print(f"accuracy = {accuracy}\n")
-        dump_results(prediction, label_test, f"{name}-results")
