0ca51e14ba866c20313870cede6f6356e822c2be
Vardiak <contact@vardiak.fr>
2022-05-02 18:44:12 +0200

    feat: hu moments

--- src/main.py
+++ src/main.py
@@ -7,18 +7,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 from sklearn.dummy import DummyClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC as SVM
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
-SHAPE_DIMENSION = 24
-SHAPE_KEYPOINTS = 200
+SHAPE_DIMENSION = 48
+SHAPE_KEYPOINTS = 96
 COLOR_DIMENSION = 24
+PONDERATION = 0
 RANDOM_SEED = 42
 N_NEIGHBORS = 1
+MODE = 'orb'
 
 def extract_color_feature_histogram(extractor, imgs):
     masks = [create_mask(img) for img in imgs]
     label_maps = [extractor.predict(img[mask]) for (img, mask) in zip(imgs, masks)]
     histograms = np.array([np.bincount(lm, minlength=len(extractor.cluster_centers_)) / len(lm) for lm in label_maps], dtype=np.float64)
     return histograms
+
+def extract_shape_feature_moments(imgs):
+    res = []
+
+    for i, img in enumerate(imgs):
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # gauss = cv2.GaussianBlur(gray_img, (3, 3), 0)
+        _,thresh = cv2.threshold(gray_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        canny = cv2.Canny(thresh, 100, 200)
+
+        plt.imsave(f'./test/{i}.png', canny, cmap='gray')
+
+        moments = cv2.moments(canny)
+
+        hu_moments = cv2.HuMoments(moments)
+
+        for j in range(0, 7):
+            hu_moments[j] = np.log10(np.abs(hu_moments[j])) if hu_moments[j] != 0 else 0
+
+        res.append(hu_moments.flatten())
+
+    res = np.array(res)
+
+    return res
 
 def extract_shape_feature_histogram(extractor, imgs):
     histograms = []
@@ -65,12 +94,15 @@ def feature_extractor(img_train):
         return kmean_shape
 
     kmean_color = color_extractor()
-    kmean_shape = shape_extractor()
-
-    return lambda img: extract_color_feature_histogram(kmean_color, img), lambda img: extract_shape_feature_histogram(kmean_shape, img)
+    if (MODE == 'orb'):
+        kmean_shape = shape_extractor()
+        return lambda img: extract_color_feature_histogram(kmean_color, img), lambda img: extract_shape_feature_histogram(kmean_shape, img)
+    else:
+        return lambda img: extract_color_feature_histogram(kmean_color, img), lambda img: extract_shape_feature_moments(img)
 
 def feature_extractor_fusion(color_feature_extractor, shape_feature_extractor):
-    return lambda img: np.hstack((color_feature_extractor(img), shape_feature_extractor(img)))
+    # return shape_feature_extractor
+    return lambda img: np.hstack((color_feature_extractor(img) * PONDERATION, shape_feature_extractor(img) * (1 - PONDERATION)))
 
 def train(classifer, feature_histogram, label_train):
     # TODO: Cross validation ?
@@ -95,10 +127,12 @@ if __name__ == "__main__":
     fused_feature_extractor = feature_extractor_fusion(color_feature_extractor, shape_feature_extractor)
 
     print("Train classifier on features ...\n")
-    classifiers = {"dummy_classifier": DummyClassifier(), "knn_classifier": KNeighborsClassifier(n_neighbors=N_NEIGHBORS)}
+    # classifiers = {"dummy_classifier" : DummyClassifier(), "knn_classifier": KNeighborsClassifier(n_neighbors=1)}
+    # classifiers = {"dummy_classifier" : DummyClassifier()}
+    classifiers = {"knn_classifier": KNeighborsClassifier(n_neighbors=N_NEIGHBORS, metric='cosine'), "svm": SVM(kernel='linear'), "forest": RandomForestClassifier(max_depth=5, random_state=RANDOM_SEED)}
 
     for name, clf in classifiers.items():
         train(clf, fused_feature_extractor(img_train_aug), label_train)
         prediction, accuracy = evaluate(clf, fused_feature_extractor(img_test), label_test)
-        print(f"accuracy = {accuracy}\n")
+        print(f"{name} accuracy = {accuracy}\n")
         dump_results(prediction, label_test, f"{name}-results")
